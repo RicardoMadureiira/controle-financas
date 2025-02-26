a6241dc9d75e350bbbe151724f067e6fcb0f5182
RicardoMadureiira <ricardomadureira.dev@gmail.com>
2025-02-26 00:09:29 -0300

    change to deploy

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,18 +9,18 @@ app.setErrorHandler((error, request, reply) => {
 });
 
 const start = async () => {
-    // Configuração de CORS ajustada para aceitar requisições do frontend
-    await app.register(cors, {
-        origin: ['http://localhost:5173', 'https://controle-financasrm.vercel.app/'], // Substitua pelo URL do seu frontend
-        methods: ['GET', 'POST', 'PUT', 'DELETE']
-    })
+   
+    await app.register(cors)
     
     await app.register(routes);
     
     try {
-        // Convertendo para número para garantir o tipo correto
+       
         const PORT = Number(process.env.PORT) || 3000;
+        
+       
         await app.listen({ port: PORT, host: '0.0.0.0' });
+        
         console.log(`Server running on port ${PORT}`);
     } catch(err) {
         console.error(err);
