ee7dd74062c87735306772fbad17ce7e153f7dc3
RicardoMadureiira <ricardomadureira.dev@gmail.com>
2025-03-04 00:27:34 -0300

    requestBody converted a number

--- backend/src/controllers/CreateCustomerController.ts
+++ backend/src/controllers/CreateCustomerController.ts
@@ -5,23 +5,27 @@ import { z } from 'zod';
 // Aqui é criado um schema para validar os dados da requisição
 const createCustomerSchema = z.object({
     details: z.string().max(20, { message: "O campo 'details' deve ter no máximo 20 caracteres"}),
-    value: z.number().positive("O valor deve ser positivo").refine((value) => {
+    value: z.coerce.number().positive("O valor deve ser positivo").refine((value) => {
         // verificar se o valor tem no máximo 2 casas decimais
         const decimalPlaces = (value.toString().split(".")[1] || '').length;
         return decimalPlaces <= 2;
     }, { message: "O valor deve ter no máximo 2 casas decimais" }),
     type: z.enum(["entrada", "saida"], { message: "O tipo deve ser 'entrada' ou 'saida'" }) 
-})
+});
+
 class CreateCustomerController {
     async handle(request: FastifyRequest, reply: FastifyReply){
         try {
             // Pré-processar o corpo da requisição para lidar com valores em formato brasileiro
             const rawBody = request.body as { details: string; value: number | string; type: "entrada" | "saida" };
 
-             // Converter o valor de string com vírgula para número com ponto
-             if (typeof rawBody.value === 'string') {
+            // Converter o valor de string com vírgula para número com ponto
+            if (typeof rawBody.value === 'string') {
                 rawBody.value = parseFloat(rawBody.value.replace(',', '.'));
             }
+
+            // Atualizar o corpo da requisição com o valor convertido
+            request.body = rawBody;
 
             // Aqui esta sendo feita a validação dos dados da requisição
             const { details, value, type } = createCustomerSchema.parse(request.body);
@@ -48,4 +52,4 @@ class CreateCustomerController {
     }
 }
 
-export { CreateCustomerController };	
+export { CreateCustomerController };
