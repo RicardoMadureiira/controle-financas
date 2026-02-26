fda6ee956b68eba48aa60e77b86f17c999f50994
Ricardo Madureira <104867612+RicardoMadureiira@users.noreply.github.com>
2026-02-26 16:10:48 -0300

    Refactor DeleteCustomerService to include anonUserId

--- backend/src/services/DeleteCustomerService.ts
+++ backend/src/services/DeleteCustomerService.ts
@@ -1,34 +1,26 @@
 import prismaClient from "../prisma";
 
 interface DeleteCustomerProps{
-    id: string;
+  id: string;
+  anonUserId: string;
 }
 
 class DeleteCustomerService{
-    async execute({ id }: DeleteCustomerProps) {
-        // deletar o cliente com ID passado
-        if(!id){
-            throw new Error("ID inválido!"); // se o ID não for passado, retornar erro
-        }
+  async execute({ id, anonUserId }: DeleteCustomerProps) {
 
-        const findCustomer = await prismaClient.customer.findFirst({
-            where: {
-                id: id // id igual ao id passado
-            }
-        })
-
-        if(!findCustomer) {
-            throw new Error("Transação não existe!"); // se o id não for encontrado no banco de dados, retornar erro
-        }
-
-        await prismaClient.customer.delete({
-            where: {
-                id: findCustomer.id // deletar o cliente com o id encontrado
-            }
-        })
-
-        return { message : "Transação apagada com sucesso!" };
+    if(!id){
+      throw new Error("ID inválido!");
     }
+
+    const customer = await prismaClient.customer.deleteMany({
+      where: {
+        id,
+        anonUserId
+      }
+    });
+
+    return { message: "Transação apagada com sucesso!" };
+  }
 }
 
 export { DeleteCustomerService };
